6f4ab26027b28d4954e362b5cbc810555e737671
Jordan Matelsky <j6k4m8@gmail.com>
2016-04-11 15:58:24 -0400

    remove version check from neurodata

--- ndio/__init__.py
+++ ndio/__init__.py
@@ -1,1 +1,15 @@
+import requests
+
+
 version = "1.0.0"
+
+def check_version():
+    """
+    Tells you if you have an old version of ndio.
+    """
+    r = requests.get('https://pypi.python.org/pypi/ndio/json').json()
+    r = r['info']['version']
+    if r != version:
+        print("A newer version of ndio is available. " +
+              "'pip install -U ndio' to update.")
+    return r
